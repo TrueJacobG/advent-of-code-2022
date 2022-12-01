a8729d16ad971d9aeab5e545a349a1a866049e3e
Jakub <jakubgradzewicz1309@gmail.com>
2022-12-01 10:44:05 +0100

    repo init

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+import solution.Day1;
+import solution.Solution;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Day1();
+        solution.solve();
+    }
+}
---
+++ src/main/java/solution/Day1.java
@@ -0,0 +1,12 @@
+package solution;
+
+public class Day1 implements Solution{
+    // Calorie Counting
+    // https://adventofcode.com/2022/day/1
+
+    @Override
+    public void solve() {
+
+    }
+
+}
